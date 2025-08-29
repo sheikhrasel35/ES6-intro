7b09c8e9b7c7a1e60f5bc5f31a5d74a3e4bbb4e9
sheikhrasel35 <abushaidrasel@gmail.com>
2025-08-29 23:33:12 +0600

    added the entries.js

---
+++ entries.js
@@ -0,0 +1,13 @@
+const employee = {
+  name: "shemul",
+  designation: "QA",
+  salary: "10000",
+  experience: 2,
+};
+
+const keys = Object.keys(employee);
+const values = Object.values(employee);
+const entries = Object.entries(employee);
+console.log(keys);
+console.log(values);
+console.log(entries);
